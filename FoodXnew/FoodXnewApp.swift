f6ed0125dd50b557bcc378ac98d59d8f2ec1e03c
FoodH <sanguny1992@gmail.com>
2026-05-10 18:56:36 +0300

    Initial Commit

---
+++ FoodXnew/FoodXnewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodXnewApp.swift
+//  FoodXnew
+//
+//  Created by Evgeny on 10.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FoodXnewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
